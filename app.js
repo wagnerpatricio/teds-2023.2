53335b4b90121163e29982b7d65d98674a65ce52
wagnerpatricio <88751653+wagnerpatricio@users.noreply.github.com>
2023-11-09 21:21:16 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Exercicio 02
+
+import React from 'react';
+import { View } from 'react-native';
+import Contador from './components/contador';
+
+const App = () => {
+  return (
+    <View>
+      <Contador />
+    </View>
+  );
+}
+
+export default App;   
